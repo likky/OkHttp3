06372a3df41b198c541cd75c81ae43df53629aa1
likky <664907696@qq.com>
2018-09-03 18:31:20 +0800

    OkHttp3 download pictures, Xamarin android

--- OkHttp3/MainActivity.cs
+++ OkHttp3/MainActivity.cs
@@ -9,6 +9,11 @@ using Square.OkHttp3;
 using Square.OkIO;
 using Java.Util.Concurrent;
 using Java.Lang;
+using System.IO;
+using Android.Support.V4.App;
+using Android;
+using Android.Content.PM;
+using Android.Net;
 
 namespace OkHttp
 {
@@ -17,6 +22,11 @@ namespace OkHttp
     {
         private OkHttpClient client;
         string url = "https://www.google.com.hk/";
+        string url2 = "https://englishpicture.oss-cn-shanghai.aliyuncs.com/fruit/apple.jpg";
+        string pictureFolder;
+        Java.IO.File pictureFile;
+        string filename;
+        int PERMISSION_WriteExternalStorage = 1;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,7 +34,16 @@ namespace OkHttp
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             var button1 = FindViewById(Resource.Id.button1) as Button;
+            var button2 = FindViewById(Resource.Id.button2) as Button;
             var textview1 = FindViewById(Resource.Id.textView1) as TextView;
+            var image1 = FindViewById(Resource.Id.imageView1) as ImageView;
+            pictureFolder = GetCardPath() + "/okhttp3";
+            pictureFile= new Java.IO.File(pictureFolder);
+            if (!pictureFile.Exists())
+            {
+                pictureFile.Mkdirs();
+            }
+            filename= System.IO.Path.Combine(pictureFolder, "apple.jpg");
 
             button1.Click += async delegate
             {
@@ -34,18 +53,60 @@ namespace OkHttp
                     string x = await Get(url);
                     textview1.Text = x;
                 }
-                catch(IOException e)
+                catch(Java.IO.IOException e)
                 {
                     e.PrintStackTrace();
                 }
 
-
             };
 
+            button2.Click += async delegate
+            {
+                //下载并保存图片到okhttp3文件夹，显示图片。实际项目中已下载不需要重复下载，这里忽略。
+                byte[] bytes =  await GetPicture(url2);
+                if (bytes != null)
+                {
+                    FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+                    await fs.WriteAsync(bytes, 0, bytes.Length);
+
+                    //Console.WriteLine("localPath:" + localpath3);
+                    fs.Close();
+                    Uri uri = Uri.FromFile(new Java.IO.File(filename));
+                    //Uri uri = Uri.Parse(filename);
+                    image1.SetImageURI(uri);
+                }
+            };
+            //动态申请存储权限
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.M)
+            {
+                bool isGrant = ActivityCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) == Permission.Granted;
+                if (isGrant == false)
+                {
+                    ActivityCompat.RequestPermissions(this, new string[] { Manifest.Permission.WriteExternalStorage }, PERMISSION_WriteExternalStorage);
+                }
+            }
 
         }
 
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            if (requestCode == PERMISSION_WriteExternalStorage)
+            {
+                if (grantResults[0] == Permission.Granted)
+                {
+                    if (Build.VERSION.SdkInt != Android.OS.BuildVersionCodes.NMr1)
+                        Toast.MakeText(this, "存储权限已申请", ToastLength.Short).Show();
+                }
+                else
+                {
+                    if (Build.VERSION.SdkInt != Android.OS.BuildVersionCodes.NMr1)
+                        Toast.MakeText(this, "申请存储权限被拒，无法存储铃声", ToastLength.Short).Show();
+                }
+            }
+
+        }
         //Android3.0 以后已经不允许在主线程访问网络，报错android.os.NetworkOnMainThreadException
+        //获取网页内容
         public async System.Threading.Tasks.Task<string> Get(string url)
         {
             //连接超时5秒
@@ -60,7 +121,7 @@ namespace OkHttp
             Response response = await client.NewCall(request).ExecuteAsync();
             if (response.Code() == 200)
             {
-                Toast.MakeText(this, "response.Code() == 200", ToastLength.Short).Show();
+                //Toast.MakeText(this, "response.Code() == 200", ToastLength.Short).Show();
                 var content = response.Body().String();
 
                 return content;
@@ -68,6 +129,34 @@ namespace OkHttp
             else
                 return "null";
  
+        }
+
+        //下载图片
+        public async System.Threading.Tasks.Task<byte[]> GetPicture(string url)
+        {
+            client = new OkHttpClient.Builder()
+            .ConnectTimeout(5, TimeUnit.Seconds)
+            .WriteTimeout(5, TimeUnit.Seconds)
+            .ReadTimeout(5, TimeUnit.Seconds)
+            .Build();
+            Request request = new Request.Builder().Url(url).Build();
+            Response response = await client.NewCall(request).ExecuteAsync();
+            if (response.Code() == 200)
+            {
+                var bytes = await response.Body().BytesAsync();
+
+                return bytes;
+            }
+            else
+                return null;
+
+        }
+
+        private string GetCardPath()
+        {
+            Java.IO.File sdDir = Android.OS.Environment.ExternalStorageDirectory;//获取手机根目录             
+            return sdDir.ToString();
+
         }
     }
 }
